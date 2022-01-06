887a3e7550602439bbedb3fb56f294141a058a90
rokuosan <85651386+rokuosan@users.noreply.github.com>
2022-01-07 00:41:09 +0900

    Add generate random numbers function

--- src/main/kotlin/Client.kt
+++ src/main/kotlin/Client.kt
@@ -1,23 +1,41 @@
 import react.dom.render
 import kotlinx.browser.document
 import kotlinx.browser.window
+import react.dom.ReactHTML.div
+import react.dom.ReactHTML.h1
 
 fun main() {
     window.onload={
-        render(document.getElementById("header")) {
-            child(Welcome::class) {
-                attrs {
-                    name="Kotlin/JS"
-                }
-            }
-        }
         render(document.getElementById("question")){
-            child(Question::class){
+            div{
                 attrs{
-                    name="押してみて"
+                    className="row justify-content-md-center"
+                }
+                div{
+                    attrs{
+                        className="col-lg-5"
+                    }
+                    h1{
+                        +"OneHundred Poems"
+                    }
+                    child(Question::class){
+                        attrs{
+                            name="乱数を再生成"
+                        }
+                    }
                 }
             }
-            child(app)
+            div{
+                attrs{
+                    className="row justify-content-md-center"
+                }
+                div{
+                    attrs{
+                        className="col-lg-5"
+                    }
+                    child(test)
+                }
+            }
         }
     }
 }
--- src/main/kotlin/Question.kt
+++ src/main/kotlin/Question.kt
@@ -3,15 +3,17 @@ import react.RBuilder
 import react.RComponent
 import react.State
 import react.dom.ButtonType
-import react.dom.InputType
 import react.dom.ReactHTML.button
-import react.dom.ReactHTML.div
-import react.dom.ReactHTML.form
-import react.dom.ReactHTML.h1
-import react.dom.ReactHTML.input
+import react.dom.ReactHTML.table
+import react.dom.ReactHTML.tbody
+import react.dom.ReactHTML.td
+import react.dom.ReactHTML.th
+import react.dom.ReactHTML.thead
+import react.dom.ReactHTML.tr
 import react.dom.label
 import react.functionComponent
-import react.useState
+import kotlin.math.abs
+import kotlin.random.Random
 
 external interface QuestionProps: Props {
     var name: String
@@ -32,55 +34,119 @@ class Question(props: QuestionProps): RComponent<QuestionProps, QuestionState>(p
                 }
                 className="btn btn-primary"
                 onClick={ _ ->
-                    setState(
-                        QuestionState(name="押されたわ...")
-                    )
+//                    setState(
+//                        QuestionState(name="押されたわ...")
+//                    )
+
                 }
             }
         }
     }
 }
 
-data class Poem(
-    var id: Long=0,
-    var topContent: String = "",
-    var underContent: String = "",
-    var author: String = ""
-)
-
-var mockPoemList = listOf(Poem(topContent = "かくとだに"), Poem(topContent = "あけぬれば"))
-
-val app = functionComponent<Props> {
-    val (poemList, setPoemList) = useState(mockPoemList)
-    val (inputText, setInputText) = useState("")
-
-    div{
-        form{
-            attrs{
-                onSubmit={
-                    setPoemList(poemList.toMutableList().apply {
-                        add(Poem(topContent = inputText))
-                    })
-                    setInputText("")
-                    it.preventDefault()
+val test = functionComponent<Props> {
+    table {
+        attrs{
+            className="table table-sm table-dark table-striped container-sm table-bordered"
+        }
+        thead{
+            tr{
+                th{
+                    attrs{
+                        scope="col-sm-1"
+                    }
+                    +"#"
+                }
+                th{
+                    attrs{
+                        scope="col-sm-auto"
+                    }
+                    +"Generated Value"
                 }
             }
-            input{
-                attrs{
-                    type=InputType.text
-                    value=inputText
-                    onChange={
-                        setInputText(it.target.value)
+        }
+        tbody{
+            val numbers = getRandomList()
+            for(i in numbers.indices){
+                tr{
+                    th{
+                        attrs{
+                            scope="row text-center"
+                        }
+                        +"${i + 1}"
+                    }
+                    td{
+                        +"${numbers[i]}"
                     }
                 }
             }
-            input{
-                attrs{
-                    type=InputType.submit
-                    value="提出"
-                    disabled=inputText.isBlank()
-                }
-            }
         }
     }
 }
+
+fun getRandomList(count: Int=10, duplicate: Boolean=true): IntArray{
+//    val poemList: ArrayList<Poem> = arrayListOf()
+//    var data: Poem
+//
+//    for(p in Poems.values()){
+//        data = p.toPoem()
+//        poemList.add(data)
+//    }
+    // リスト作成
+    val randomList = arrayListOf<Int>()
+
+    // 繰り返し処理
+    repeat(count){
+        randomList.add(abs(Random.nextInt()) % 10 + 1)
+    }
+
+    if(!duplicate){
+        randomList.distinct()
+    }
+
+    return randomList.toIntArray()
+
+}
+
+//var mockPoemList = listOf(Poem(topContent = "かくとだに"), Poem(topContent = "あけぬれば"))
+//
+//val app = functionComponent<Props> {
+//    val (poemList, setPoemList) = useState(mockPoemList)
+//    val (inputText, setInputText) = useState("")
+//
+//    div{
+//        form{
+//            attrs{
+//                onSubmit={
+//                    setPoemList(poemList.toMutableList().apply {
+//                        add(Poem(topContent = inputText))
+//                    })
+//                    setInputText("")
+//                    it.preventDefault()
+//                }
+//            }
+//            input{
+//                attrs{
+//                    type=InputType.text
+//                    value=inputText
+//                    onChange={
+//                        setInputText(it.target.value)
+//                    }
+//                }
+//            }
+//            input{
+//                attrs{
+//                    type=InputType.submit
+//                    value="提出"
+//                    disabled=inputText.isBlank()
+//                }
+//            }
+//
+//            poemList.forEachIndexed{ _, poem ->
+//                div{
+//                    + poem.topContent
+//                }
+//            }
+//        }
+//    }
+//}
