3c1073bd190fdc80c8aca600582314d6c5c923c3
rokuosan <85651386+rokuosan@users.noreply.github.com>
2022-01-08 23:59:10 +0900

    Fix appearance

--- src/main/kotlin/components/Poems.kt
+++ src/main/kotlin/components/Poems.kt
@@ -9,8 +9,8 @@ data class Poem(
 
 enum class Poems(private val id: Long, private val top: String, private val under: String, private val remind: String){
 
-    KAKUTDANI(51, "かくとだに", "さしもしらじな", "かく さ(書くさ)"),
-    AKENUREBA(52, "明けぬれば", "なほ恨めしき", "あけ なほうら(開けな ほら)"),
+    KAKUTDANI(51, "かくとだに", "さしもしらじな", "かくさ(書くさ)"),
+    AKENUREBA(52, "明けぬれば", "なほ恨めしき", "あけなほうら(開けな ほら)"),
     NAGEKITUTU(53, "嘆きつつ", "いかに久しき", "嘆き イカ"),
     WASUREZINO(54, "忘れじの", "今日を限りの", "忘れじ今日を"),
     TAKINOOTOHA(55, "滝の音は", "名こそ流れて", "滝の名こそ"),
