6da8138cad6d660750451ea200f2d8297323b017
rokuosan <85651386+rokuosan@users.noreply.github.com>
2022-01-09 00:00:24 +0900

    Change appearance

--- src/main/kotlin/Client.kt
+++ src/main/kotlin/Client.kt
@@ -1,56 +1,12 @@
-import components.randomGenerator
-import components.simplePoemApp
+import components.app
 import react.dom.render
 import kotlinx.browser.document
 import kotlinx.browser.window
-import react.dom.ReactHTML.div
 
 fun main() {
     window.onload={
         render(document.getElementById("simpleApp")){
-            div{
-                attrs{
-                    className="row justify-content-md-center"
-                }
-                div{
-                    attrs{
-                        className="col-lg-5"
-                    }
-                    child(simplePoemApp)
-                }
-            }
-        }
-
-        render(document.getElementById("Components.getRandomGenerator")){
-//            div{
-//                attrs{
-//                    className="row justify-content-md-center"
-//                }
-//                div{
-//                    attrs{
-//                        className="col-lg-5"
-//                    }
-//                    h1{
-//                        +"OneHundred Components.Poems"
-//                    }
-//                    child(Question::class){
-//                        attrs{
-//                            name="乱数を再生成"
-//                        }
-//                    }
-//                }
-//            }
-            div{
-                attrs{
-                    className="row justify-content-md-center"
-                }
-                div{
-                    attrs{
-                        className="col-lg-5"
-                    }
-                    child(randomGenerator)
-                }
-            }
+            child(app)
         }
     }
 }
